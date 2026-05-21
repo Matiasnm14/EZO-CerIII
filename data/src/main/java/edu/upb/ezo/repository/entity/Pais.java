2ca746f7292291ebe1ae14f1dd5fcc5910a07d72
matiasnm14 <matiasnm14@gmail.com>
2026-05-21 14:53:02 -0400

    Cosas en Pais que hay que agregar a las demas entities

--- data/src/main/java/edu/upb/ezo/repository/entity/Pais.java
+++ data/src/main/java/edu/upb/ezo/repository/entity/Pais.java
@@ -1,13 +1,13 @@
 package edu.upb.ezo.repository.entity;
 
+import jakarta.annotation.Nonnull;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.util.UUID;
@@ -21,6 +21,8 @@ public class Pais {
     @UuidGenerator
     private UUID id;
 
+    @NotBlank(message = "El pais no debe de estar vacio")
+    @NotNull(message = "El pais no debe de ser nulo")
     @Column(name = "nombre",length = 50,nullable = false)
     private String nombre;
 
