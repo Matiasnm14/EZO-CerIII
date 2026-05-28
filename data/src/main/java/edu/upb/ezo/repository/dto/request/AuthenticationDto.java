c17f4ba125bf1a69707509787baeaec3d4aed272
Bestga14 <gaelfcl14@gmail.com>
2026-05-28 17:27:36 -0400

    Implementacion de autenticacion

---
+++ data/src/main/java/edu/upb/ezo/repository/dto/request/AuthenticationDto.java
@@ -0,0 +1,5 @@
+package edu.upb.ezo.repository.dto.request;
+
+
+public record AuthenticationDto (String username, String password) {
+}
---
+++ data/src/main/java/edu/upb/ezo/repository/enums/RolType.java
@@ -0,0 +1,6 @@
+package edu.upb.ezo.repository.enums;
+
+public enum RolType {
+    ROL_ADMIN_EZO,
+    ROL_USUARIO,
+}
